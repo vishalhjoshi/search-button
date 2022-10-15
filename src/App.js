9ba3425cd02f35ef12c363bf010a28ea080a8fb2
Vishal J <vishalj@ideyalabs.com>
2022-10-15 18:08:32 +0530

    added search button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={() => setOpen(!open)} className="container">
+      <div className={`btn-container ${open ? "btn-open" : "btn-close"}`}>
+        <button className="btn"> Search</button>
+      </div>
     </div>
   );
 }
